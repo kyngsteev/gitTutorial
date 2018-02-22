78182de46f916874e407764d45b76cb9f9a15cfa
kyngsteev <stephenomoarukhe@gmail.com>
2018-02-22 23:24:54 +0100

    updated html and js files

--- HTML-CSS/script.js
+++ HTML-CSS/script.js
@@ -1,10 +1,11 @@
-alert('Hello World');
-function searchGoogle(){
-  var searchValue = document.getElementById('formInput').value;
-
-  if(typeof searchValue === undefined || searchValue === null){
-    alert('Bummer!!! You did not input anything in search box');
-  }else{
-    alert('Hurray!!! You are searching for ' + searchValue);
+$(document).ready(function(){
+  function searchGoogle(){
+    var searchValue = document.getElementById('formInput').value;
+  
+    if(typeof searchValue === undefined || searchValue === null){
+      alert('Bummer!!! You did not input anything in search box');
+    }else{
+      alert('Hurray!!! You are searching for ' + searchValue);
+    }
   }
-}
+});
