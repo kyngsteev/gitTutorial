6ce64195d43d8e4ba1bc278f76eae4506f479ccb
kyngsteev <stephenomoarukhe@gmail.com>
2018-02-23 17:01:49 +0100

    updated script and index file

--- HTML-CSS/script.js
+++ HTML-CSS/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   function searchGoogle(){
     var searchValue = document.getElementById('formInput').value;
   
-    if(typeof searchValue === undefined || searchValue === null){
+    if(typeof searchValue === undefined || searchValue === ""){
       alert('Bummer!!! You did not input anything in search box');
     }else{
       alert('Hurray!!! You are searching for ' + searchValue);
